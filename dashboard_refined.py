3c19f07235f698d4d001a5a063a6c3e0095d285e
Isabel von Ah <isabelvonah@gmail.com>
2023-01-04 10:37:46 +0100

    rangeSlider + Beschriftungen

--- dashboard_refined.py
+++ dashboard_refined.py
@@ -19,8 +19,9 @@ options.append({'label': 'Select All', 'value': "all"})
 app.layout = html.Div([
 
     html.Div([
+        html.Img(src=r"assets/Logo.png", style={"height": "6vh",'display': 'inline-block', 'margin-right': '2vw'}),
         html.H1("Weltmeisterschaften im Damenfussball", style = {'text-align': 'left', 'font-weight': 'bold', 'color': 'white', 'display': 'inline-block '}),
-        html.Img(src=r"assets/Logo.png", style={"right": "2vw", "position": "absolute",})
+        
     ], style={"padding": "1vw", "margin": "1vw", "background-color": "orange","height": "10vh"}),
 
     html.Div([
@@ -35,9 +36,9 @@ app.layout = html.Div([
                 style={"padding-left":"15%", "padding-top":"10%", "padding-bottom": "10%"}
                 ),
 
-            html.H5("Möchtest du sogar einzelne Länder untersuchen? Dann wähle diese aus", style = {'text-align': 'left'}),
+            html.H5("gespielte Matches pro Turnier", style = {'text-align': 'left'}),
             
-            dcc.Slider(id='matches_played', min=0, max=8, value=3, step=1,)
+            dcc.RangeSlider(id='matches_played', min=3, max=8, value=[3,8], step=1,)
             
         ], style={"width": "15vw", "height": "78vh", "padding": "1vw", "background-color": "lightgrey"} ),
 
@@ -73,38 +74,49 @@ def update_graph(option_slctd, option_slctd2):
         dff = dff
     else:
         dff = dff[dff.continent.isin(option_slctd)]
-    dff = dff[dff["matches_played"] <= option_slctd2]
+    dff = dff[(dff["matches_played"] <= option_slctd2[1]) & (dff["matches_played"] >= option_slctd2[0])]
 
 # Plotly express
-    fig_g = px.scatter(dff, x="min_playing_time", y="goals", 
-                    size="age", 
-                    color="continent", 
+    fig_g = px.scatter(dff, x="min_playing_time", y="goals", color="continent",
+                    labels={
+                     "min_playing_time": "Gesamtspielzeit im Turnier in Minuten",
+                     "goals": "Anzahl Tore",
+                     "continent": "Kontinente"
+                    },
+                    size="age",
                     hover_data=['squad'], 
                     trendline="ols",
                     trendline_scope="overall",
-                    labels={
-                        "min_playing_time": "time played",
-                        "goals": "goals",},
                     title="Tore pro Spielzeit",
                     template="simple_white"
                     )
     
-    fig_a = px.histogram(dff, x="age", title="Alter der Mannschaften", template="simple_white")
+    fig_a = px.histogram(dff, x="age",
+                    labels={
+                        "age": "Durchschnittsalter"
+                    },
+                    title="Durchschnittsalter der ausgewählten Mannschaften", template="simple_white"
+                    ).update_layout(yaxis_title="Anzahl Mannschaften")
 
     fig_c = px.bar(dff, x='squad', y='penalty_kicks_attempted',
                     labels={
-                        "squad": "team",
-                        "penalty_kicks_attempted": "penaltys",},
+                        "squad": "Mannschaft",
+                        "penalty_kicks_attempted": "Elfmeterschüsse",
+                        "continent": "Kontinente"
+                    },
                     title="Elfmeterschüsse",
                     color="continent",
                     template="simple_white"
                     )
     
     fig_m = px.line(dff, x="year", y="min_playing_time", color='squad',
-                    labels={"squad": "team",
-                        "matches_played": "Anzahl Spiele"},
+                    labels={"squad": "Mannschaft",
+                        "min_playing_time": "Gesamtspielzeit im Turnier in Minuten",
+                        "year": "Jahre"
+                    },
                     title="Spiele pro Team",
-                    template="simple_white")
+                    template="simple_white"
+                    )
 
     return fig_g, fig_a, fig_c, fig_m
 
