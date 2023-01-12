e4d0c7e949475ad93a3307976acd1349cbbbef69
Isabel von Ah <isabelvonah@gmail.com>
2023-01-12 15:17:19 +0100

    Anpassungen Layout (Kerstin)

--- dashboard_refined.py
+++ dashboard_refined.py
@@ -19,15 +19,19 @@ options.append({'label': 'Select All', 'value': "all"})
 app.layout = html.Div([
 
     html.Div([
-        html.Img(src=r"assets/Logo.png", style={"height": "6vh",'display': 'inline-block', 'margin-right': '2vw'}),
-        html.H1("Weltmeisterschaften im Damenfussball", style = {'text-align': 'left', 'font-weight': 'bold', 'color': 'white', 'display': 'inline-block '}),
+        html.Img(src=r"assets/Logo.png", style = {"height": "16vh", 'margin-left': '4vw', 'margin-top': '-0.5vw', 'display': 'inline-block', 'margin-right': '10vw'}), # >---- Position Headline geändert und Größe Logo
+        html.H1("Weltmeisterschaften im Damenfussball", style = {'text-align': 'left', 'margin-left': '-1.5vw', 'font-weight': 'bold', 'font-size': '45pt', 'color': 'white', 'display': 'inline-block '}), #---> Schriftgröße Headline geändert
         
-    ], style={"padding": "1vw", "margin": "1vw", "background-color": "orange","height": "10vh"}),
+    ], style={"padding": "1vw", "margin": "1vw", "background-color": "orange","height": "10vh"}), #das ist der orangene Balken
+
+     html.Div([
+        html.H1("von 1991 bis 2019", style = {'text-align': 'left', 'margin-top': '-1vw', 'margin-left': '21vw', 'font-weight': 'bold', 'font-size': '30pt', 'color': 'orange'}),
+    ],), #----> neues Div für Subheadline eingefügt
 
     html.Div([
 
         html.Div([
-            html.H5("Wähle Kontinente aus, die du vergleichen möchtest", style = {'text-align': 'left'}),
+            html.H5("Wähle Kontinente aus, die Du vergleichen möchtest", style = {'text-align': 'left', 'color': 'white'}), # ---> Rechtschreibung korrigiert und Fond Farbe an Header angepasst.
 
             dcc.Checklist(id='conti',
                 options=[{'label':x , 'value':x} for x in all],
@@ -36,11 +40,11 @@ app.layout = html.Div([
                 style={"padding-left":"15%", "padding-top":"10%", "padding-bottom": "10%"}
                 ),
 
-            html.H5("gespielte Matches pro Turnier", style = {'text-align': 'left'}),
+            html.H5("Gespielte Matches pro Turnier", style = {'text-align': 'left', 'color': 'white'}), #---> Rechtschreibung korrigiert und Fond Farbe an Header angepasst.
             
             dcc.RangeSlider(id='matches_played', min=3, max=8, value=[3,8], step=1,)
             
-        ], style={"width": "15vw", "height": "78vh", "padding": "1vw", "background-color": "lightgrey"} ),
+        ], style={"width": "16vw", "height": "78vh", 'margin-top': '2vw', "padding": "1vw", "background-color": "orange"} ), # ---> grauer Balken geändert in orange und Breite um 1 vergrößert, damit Logo drüber genau in der Mitte ist. Abstand zum oberen Balken vergrößert.
 
         html.Div([
             dcc.Graph(id='goals', figure={}, style={'display': 'inline-block', 'width': '37vw', 'height': '40vh'}),
@@ -51,7 +55,7 @@ app.layout = html.Div([
 
             dcc.Graph(id='match', figure={}, style={'display': 'inline-block', 'width': '37vw', 'height': '40vh'})
 
-        ], style={"margin-left": "18vw", "transform": "translateY(-78vh)"}),
+        ], style={"margin-left": "18vw", 'margin-top': '-1vw', "transform": "translateY(-78vh)"}), #----> Höhe der Graphen an die Schrifthöhe im linken Balken angepasst.
         
     ],
     style={"margin-left": "1vw", "margin-top": "1vh", "margin-right": "5vw"}),
